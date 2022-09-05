b8a5f81647a2f5ed3e8e068698b8a9197f0a0f93
Jaco Swarts <jaco@productive.me>
2022-09-05 06:37:55 +0200

    Add npm dependency to simpl-schema

--- package.js
+++ package.js
@@ -3,10 +3,14 @@ Package.describe({
   version: '1.3.8',
   summary: 'Publish aggregations reactively',
   git: 'https://github.com/robfallows/tunguska-reactive-aggregate',
-  documentation: 'README.md'
+  documentation: 'README.md',
 });
 
-Package.onUse(function(api) {
+Npm.depends({
+  'simpl-schema': '1.12.3',
+});
+
+Package.onUse(function (api) {
   api.versionsFrom('1.5');
   api.use('mongo');
   api.use('ecmascript');
